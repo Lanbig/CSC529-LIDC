f3eecf6464a1d9432c545a7b3e401217ae2434a3
Lanbig <Lanbig@loop-depaulsecure-231-59.depaulsecure-student.depaul.edu>
2016-03-01 16:26:44 -0600

    Add
    - Visualize data distribution
    - confusion matrix and class performance evaluation

--- code.m
+++ code.m
@@ -21,6 +21,10 @@ Y = malignAvg; % dependent variables
 
 cv = cvpartition(n, 'holdout', 0.34);
 
+%% Visualize data distribution
+figure(1),histogram(Y)
+figure(2),histogram(Y(cv.training,:))
+figure(3),histogram(Y(cv.test,:))
 
 %% Model Building
 
@@ -31,6 +35,12 @@ view(dt,'mode','graph')
 resubLoss(dt)
 loss(dt,X(cv.test,:),Y(cv.test,:))
 
+yHat = predict(dt,X(cv.training,:));
+CP = classperf(Y(cv.training,:),yHat);
+confusionmat(Y(cv.training),predict(dt,X(cv.training,:)))
+confusionmat(Y(cv.test),predict(dt,X(cv.test,:)))
+
+
 %% Tree - Bagging
 tic
 bagTree = fitensemble(X(cv.training,:),Y(cv.training,:),'bag',200,'Tree','Type','classification','nprint',100);
@@ -38,6 +48,9 @@ toc
 
 resubLoss(bagTree)
 loss(bagTree,X(cv.test,:),Y(cv.test,:))
+
+confusionmat(Y(cv.training),predict(bagTree,X(cv.training,:)))
+confusionmat(Y(cv.test),predict(bagTree,X(cv.test,:)))
 
 %% Tree - AdaboostM2
 tic
